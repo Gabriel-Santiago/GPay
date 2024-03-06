1f5dba0563806d76ffb1d6b0f7b2a5a975565a20
Gabriel <gabrielsrmj@alu.ufc.br>
2024-03-06 08:11:25 -0300

    Base do Projeto

---
+++ src/test/java/BancoDigital/GPay/GPayApplicationTests.java
@@ -0,0 +1,13 @@
+package BancoDigital.GPay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GPayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
